4e36a9746afaaff39cecf581780bb178bee70db7
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-09-12 18:51:26 +0200

    feat(modules/user): enforce setting user email and name
    Adds assertions to ensure that either a default email/name is
    defined or all profiles define a user email/name. This ensures
    necessary user information is available.

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -1,6 +1,7 @@
-{ lib, ... }@args:
+{ config, lib, ... }@args:
 
 let
+  cfg = config.programs.nixkraken;
   options = import ./options.nix args;
 in
 {
@@ -12,5 +13,25 @@ in
     description = ''
       User settings.
     '';
+  };
+
+  config = lib.mkIf (cfg.enable) {
+    # Since the user submodule is shared by both top-level and profiles, we cannot rely on module type checks and
+    # must instead resort to use an assertion on the resolved configuration data
+    assertions =
+      let
+        allProfilesHaveUserAttr =
+          attr: lib.foldl' (acc: { user }: acc && user.${attr} != null) true cfg.profiles;
+      in
+      [
+        {
+          assertion = cfg.user.email != null || allProfilesHaveUserAttr "email";
+          message = "Either a default email (`user.email`) must be defined or all profiles must define a user email (`profile.user.email`)";
+        }
+        {
+          assertion = cfg.user.name != null || allProfilesHaveUserAttr "name";
+          message = "Either a default name (`user.name`) must be defined or all profiles must define a user name (`profile.user.name`)";
+        }
+      ];
   };
 }
