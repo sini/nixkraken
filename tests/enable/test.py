df0706e4cb436f9058dde18a91783efb93ba5d7d
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-09-12 19:23:38 +0200

    test(enable): ensure eula popup is displayed and update screenshot filename

--- tests/enable/test.py
+++ tests/enable/test.py
@@ -8,12 +8,16 @@ with subtest("GitKraken launches"):
 
     # Wait for window to show up
     # WARN: for some reason, this succeeds a few seconds before the window actually
-    #       shows up on screen, hence the 10 seconds sleep
+    #       shows up on screen, hence the 15 seconds sleep
     machine.wait_for_window("GitKraken Desktop")
-    machine.sleep(10)
+    machine.sleep(15)
+
+    # Check EULA popup is displayed on screen
+    # This is a strong indicator that the app successfully launched in the right state
+    machine.wait_for_text("agree to our EULA")
 
     # Take a screenshot of GitKraken
-    machine.screenshot("gitkraken")
+    machine.screenshot("gitkraken-enable")
 
 with subtest("GitKraken config exists"):
     machine.succeed("stat ~/.gitkraken/config")
