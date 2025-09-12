1e10ec4d0bef0a5d97b620cdfefef1b3987b05ec
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-09-12 19:24:30 +0200

    test: add specific version test

---
+++ tests/specific-version/default.nix
@@ -0,0 +1,36 @@
+{
+  pkgs ? import <nixpkgs> {
+    config.allowUnfreePredicate =
+      pkg:
+      builtins.elem (pkgs.lib.getName pkg) [
+        "gitkraken"
+      ];
+
+    overlays = [ ];
+  },
+}:
+
+let
+  lib = pkgs.lib;
+in
+pkgs.testers.runNixOSTest {
+  name = "enable";
+
+  nodes.machine = {
+    imports = [
+      ../_common
+      ../_common/dummy-user.nix
+    ];
+
+    home-manager.users.root.programs.nixkraken = {
+      enable = true;
+      version = "11.1.0";
+    };
+  };
+
+  testScript = lib.readFile ./test.py;
+
+  meta = {
+    maintainers = with lib.maintainers; [ nicolas-goudry ];
+  };
+}
