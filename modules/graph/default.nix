c4be5b417740c344b56b3704b2e51ea7f11af1c2
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-09-13 02:39:43 +0200

    fix(modules/graph): assertion to support all optional options

--- modules/graph/default.nix
+++ modules/graph/default.nix
@@ -30,22 +30,29 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    assertions = [
-      {
-        assertion =
+    assertions =
+      let
+        columnSettings = [
           cfg.graph.showAuthor
-          || cfg.graph.showDatetime
-          || cfg.graph.showMessage
-          || cfg.graph.showRefs
-          || cfg.graph.showSHA
-          || cfg.graph.showGraph;
-        message = "Commit graph cannot be empty (`graph.*`)";
-      }
-      {
-        assertion = cfg.graph.showAll -> lib.isNull (cfg.graph.maxCommits);
-        message = "Cannot set a maximum number of commits (`graph.maxCommits`) to show in commit graph if all commits are shown (`graph.showAll`)";
-      }
-    ];
+          cfg.graph.showDatetime
+          cfg.graph.showMessage
+          cfg.graph.showRefs
+          cfg.graph.showSHA
+          cfg.graph.showGraph
+        ];
+        allNull = builtins.all (setting: setting == null) columnSettings;
+        oneTrue = lib.findFirst (setting: setting == true) false columnSettings;
+      in
+      [
+        {
+          assertion = allNull || oneTrue;
+          message = "Commit graph cannot be empty (`graph.*`)";
+        }
+        {
+          assertion = cfg.graph.showAll -> lib.isNull (cfg.graph.maxCommits);
+          message = "Cannot set a maximum number of commits (`graph.maxCommits`) to show in commit graph if all commits are shown (`graph.showAll`)";
+        }
+      ];
 
     programs.nixkraken._submoduleSettings.graph = settings;
   };
