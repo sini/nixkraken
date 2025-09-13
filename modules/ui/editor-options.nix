7fe09b2288a72aad2db9650c3039c18682acdb33
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-09-13 20:59:28 +0200

    refactor(modules/ui): move cli and editor options to separate files

---
+++ modules/ui/editor-options.nix
@@ -0,0 +1,63 @@
+{ lib, ... }:
+
+{
+  eol = lib.mkOption {
+    type = lib.types.enum [
+      "CRLF"
+      "LF"
+    ];
+    default = "LF";
+    description = ''
+      End of line character to use in the editor.
+    '';
+  };
+
+  fontFamily = lib.mkOption {
+    type = with lib.types; nullOr str;
+    default = null;
+    example = "Liberation Mono";
+    description = ''
+      Font family to use in the editor.
+    '';
+  };
+
+  fontSize = lib.mkOption {
+    type = lib.types.int;
+    default = 12;
+    description = ''
+      Font size to use in the editor.
+    '';
+  };
+
+  lineNumbers = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = ''
+      Show line numbers in the editor.
+    '';
+  };
+
+  syntaxHighlight = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = ''
+      Enable syntax highlighting in the editor.
+    '';
+  };
+
+  tabSize = lib.mkOption {
+    type = lib.types.int;
+    default = 4;
+    description = ''
+      Size of the indentation in the editor.
+    '';
+  };
+
+  wrap = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = ''
+      Enable word wrap in the editor.
+    '';
+  };
+}
--- modules/ui/profile-options.nix
+++ modules/ui/profile-options.nix
@@ -1,6 +1,12 @@
 { lib, ... }:
 
+let
+  cli = import ./cli-options.nix { inherit lib; };
+  editor = import ./editor-options.nix { inherit lib; };
+in
 {
+  inherit cli editor;
+
   rememberTabs = lib.mkOption {
     type = lib.types.bool;
     default = true;
@@ -49,179 +55,6 @@
     description = ''
       UI zoom percentage.
     '';
-  };
-
-  cli = {
-    autocomplete = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = ''
-          Enable autocomplete suggestions.
-        '';
-      };
-
-      tabBehavior = lib.mkOption {
-        type =
-          with lib.types;
-          nullOr (enum [
-            "enter"
-            "ignore"
-            "navigation"
-          ]);
-        default = "ignore";
-        description = ''
-          Behavior of the tab key in the integrated terminal when autocomplete is enabled.
-
-          When set to `enter`, the highlighted suggestion will be entered.
-
-          When set to `navigation`, the next suggestion will be selected.
-
-          When set to `ignore`, the tab key will be sent to the shell.
-        '';
-      };
-    };
-
-    cursor = lib.mkOption {
-      type = lib.types.enum [
-        "bar"
-        "block"
-        "underline"
-      ];
-      default = "block";
-      description = ''
-        Style of the cursor in the integrated terminal.
-      '';
-    };
-
-    defaultPath = lib.mkOption {
-      type = with lib.types; nullOr path;
-      default = null;
-      example = lib.literalExpression "\${config.home.homeDirectory}";
-      description = ''
-        Default directory to open terminal tabs into.
-      '';
-    };
-
-    fontFamily = lib.mkOption {
-      type = with lib.types; nullOr str;
-      default = null;
-      example = "Liberation Mono";
-      description = ''
-        Font family to use in the integrated terminal.
-      '';
-    };
-
-    fontSize = lib.mkOption {
-      type = lib.types.int;
-      default = 12;
-      description = ''
-        Font size to use in the integrated terminal.
-      '';
-    };
-
-    lineHeight = lib.mkOption {
-      type = lib.types.int;
-      default = 1;
-      description = ''
-        Line height in the integrated terminal.
-      '';
-    };
-
-    graph = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = ''
-          Show graph panel by default.
-
-          > [!NOTE]
-          >
-          > This setting only affects terminal tabs when the current directory is a repository.
-        '';
-      };
-
-      position = lib.mkOption {
-        type =
-          with lib.types;
-          nullOr (enum [
-            "bottom"
-            "left"
-            "right"
-            "top"
-          ]);
-        default = "bottom";
-        description = ''
-          Default graph panel position.
-
-          > [!NOTE]
-          >
-          > This setting only affects terminal tabs when the current directory is a repository.
-        '';
-      };
-    };
-  };
-
-  editor = {
-    eol = lib.mkOption {
-      type = lib.types.enum [
-        "CRLF"
-        "LF"
-      ];
-      default = "LF";
-      description = ''
-        End of line character to use in the editor.
-      '';
-    };
-
-    fontFamily = lib.mkOption {
-      type = with lib.types; nullOr str;
-      default = null;
-      example = "Liberation Mono";
-      description = ''
-        Font family to use in the editor.
-      '';
-    };
-
-    fontSize = lib.mkOption {
-      type = lib.types.int;
-      default = 12;
-      description = ''
-        Font size to use in the editor.
-      '';
-    };
-
-    lineNumbers = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = ''
-        Show line numbers in the editor.
-      '';
-    };
-
-    syntaxHighlight = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = ''
-        Enable syntax highlighting in the editor.
-      '';
-    };
-
-    tabSize = lib.mkOption {
-      type = lib.types.int;
-      default = 4;
-      description = ''
-        Size of the indentation in the editor.
-      '';
-    };
-
-    wrap = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = ''
-        Enable word wrap in the editor.
-      '';
-    };
   };
 
   theme = lib.mkOption {
