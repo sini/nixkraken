3006b450a2704b9d564f15e96df04eb4a47af1ce
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-10-04 01:47:53 +0200

    fix(docs/theme): page toc not hidden in home when deployed

--- docs/theme/js/toc.js
+++ docs/theme/js/toc.js
@@ -1,5 +1,5 @@
 // Array of paths where the TOC should be hidden
-const hideOnPath = ['/', '/index.html', '/nixkraken/index.html']
+const hideOnPath = ['/', '/index.html', '/nixkraken/', '/nixkraken/index.html']
 // Array to store the generated table of contents elements
 const tocContent = []
 
