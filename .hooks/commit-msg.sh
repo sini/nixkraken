1313f5a0bf4b664d5e2227b90cd439f8220586ad
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-10-30 23:30:39 +0100

    chore(cog): setup hooks

--- .hooks/commit-msg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-convco check --ignore-reverts -n 1
--- shell.nix
+++ shell.nix
@@ -19,10 +19,6 @@ pkgs.mkShellNoCC {
     ++ pkgs.lib.mapAttrsToList (pkg: _: localPkgs.${pkg}) localPkgs;
 
   shellHook = ''
-    find .hooks \
-      -maxdepth 1 \
-      -type f \
-      -name '*.sh' \
-      -exec bash -c 'ln -sf "$PWD/$1" ".git/hooks/$(basename "$1" .sh)"' _ {} \;
+    cog install-hook --all --overwrite
   '';
 }
